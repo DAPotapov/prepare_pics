394d1baaa3abff05c9cd651a6869380d6580f075
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-04-07 14:25:04 +0300

    Bug fixing and errors handling

--- 2site.sh
+++ 2site.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/bash
 
-# This script prepare images (change size and place watermark)
-# to be published on currated Wordpress site.
-# It accept folder name to be processed.
-# File with watermark should be in same directory as this script
-
-if [ ! -n "$1" ]; then
-    echo "Provide a folder name"
-    exit 1
+if [ -z "$1" ]; then
+    echo "This script prepares images (changes size and adds watermark)";
+    echo "to be published on Wordpress site.";
+    echo "Syntax: ./2site.sh <folder name>";
+    exit 1;
 fi
 
 SOURCE="$1"
@@ -15,33 +12,42 @@ SMALL="$SOURCE"_sm
 PUBLISH="$SOURCE"_publish
 # This value set for vertical size in wordpress settings
 VSIZE=1024
+# WM=nofile.png # for testing purposes
 WM=watermark.png
 
 if [ -d "$SOURCE" ]; then
     cd "$SOURCE" || exit 1;
     if [ ! -d "$SMALL" ]; then
-        mkdir "$SMALL";
+        mkdir "$SMALL" || { echo "Couldn't create folder '$SMALL'. Maybe don't have enough permissions."; exit 1; }
     fi
-    mogrify -resize x$VSIZE -path "$SMALL" ./*.jpg;
+    pics=( ./*.jpg );
+    mogrify -resize x$VSIZE -path "$SMALL" "${pics[@]}" || { echo "Couldn't proceed files"; rm -r "$SMALL"; exit 1; }
 else
-    echo "$SOURCE" directory does not exist;
+    echo "'$SOURCE' directory does not exist";
+    exit 1;
 fi
 
 if [ -f ../"$WM" ]; then
-    if [ -d "$SMALL" ]; then
         if [ ! -d "$PUBLISH" ]; then
-            mkdir "$PUBLISH"
+            mkdir "$PUBLISH" || exit 1;
         fi
-        cd "$SMALL" || exit 1;
-        for f in ./*.jpg
+        cd "$SMALL" || { echo "'$SMALL' folder dissapeared!!"; exit 1;}
+        for f in ./*.jpg 
             do
-                echo Adding watermark to "$f";
-                composite -gravity south ../../$WM "$f" ../"$PUBLISH"/"$f"; # добавить сюда экзит с эчо на случай ошибки
-            done;
+                if [ -f "$f" ]; then                
+                    echo "Adding watermark to '$f'";
+                    composite -gravity south ../../$WM "$f" ../"$PUBLISH"/"$f" ||  { echo "Couldn't add watermark."; exit 1; }
+                else
+                    echo "File '$f' not found";
+                    exit 1;
+                fi
+            done
         cd ../..;
-        mv "$SOURCE" "$SOURCE"_done; # add check for source directory just in case?
-        echo "Operation completed";
-    fi
+        if mv "$SOURCE" "$SOURCE"_done
+            then echo "Operation completed";
+        else echo "'$SOURCE' folder rename failed";
+            exit 1;
+        fi        
 else
-    echo File $WM containing watermark must be in the same directory as this script;
+    echo "File '$WM' containing watermark must be in the same directory as this script";
 fi
