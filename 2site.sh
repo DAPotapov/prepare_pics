40c30b3ffc78f9f06bd1e91cd760ebb9b2a5c2c6
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-04-07 19:40:46 +0300

    Resize now work only for larger images

--- 2site.sh
+++ 2site.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 if [ -z "$1" ]; then
-    echo "This script prepares images (changes size and adds watermark)";
-    echo "to be published on Wordpress site.";
+    echo "This script prepares images (changes size and adds watermark) to be published on Wordpress site.";
+    echo "Modified files saved in sub-folders.";
+    echo "Existing files with same names will be overwritten.";
+    echo 
     echo "Syntax: ./2site.sh <folder name>";
     exit 1;
 fi
@@ -20,8 +22,18 @@ if [ -d "$SOURCE" ]; then
     if [ ! -d "$SMALL" ]; then
         mkdir "$SMALL" || { echo "Couldn't create folder '$SMALL'. Maybe don't have enough permissions."; exit 1; }
     fi
-    pics=( ./*.jpg );
-    mogrify -resize x$VSIZE -path "$SMALL" "${pics[@]}" || { echo "Couldn't proceed files"; rm -r "$SMALL"; exit 1; }
+    for f in ./*.jpg
+        do 
+            height=$(identify -format '%h' "$f")
+            if [ "$height" -gt "$VSIZE" ] 
+            then
+                echo "Resizing '$f'";
+                mogrify -resize x$VSIZE -path "$SMALL" "$f" || { echo "Couldn't resize file '$f'."; exit 1; }
+            else
+                echo "File '$f' height ('$height') is lower than '$VSIZE'. Left unchanged.";
+                cp "$f" "$SMALL";
+            fi
+        done
 else
     echo "'$SOURCE' directory does not exist";
     exit 1;
